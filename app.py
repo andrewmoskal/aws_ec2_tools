6e57d243748c9ed4c7ef07bb12199479c993b2e9
andrewmoskal <andrey.moskal@proton.me>
2023-07-03 09:12:15 +0300

    Fix

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
 import boto3
 import os
+import httpx
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 
-def get_client(_region, _type='ec2') -> boto3.client:
-    return boto3.client(
-        _type,
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=_region
-    )
+class AwsEc2:
 
+    def __init__(self):
+        self.__client = self.get_client(_type='ssm')
 
-def get_region_name(_region):
-    __client = get_client('eu-north-1', 'ssm')
-    __name = f'/aws/service/global-infrastructure/regions/{_region}/longName'
-    __response = __client.get_parameter(Name=__name)
-    return __response.get('Parameter').get('Value')
+    def get_client(self, _region='eu-north-1', _type='ec2') -> boto3.client:
 
+        return boto3.client(
+            _type,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name=_region
+        )
 
-def get_regions() -> dict:
-    __client = get_client('eu-north-1')
-    __response = __client.describe_regions()
-    __regions = {}
-    [__regions.update({x.get('RegionName'): get_region_name(x.get('RegionName'))}) for x in __response.get('Regions')]
-    return __regions
+    def get_region_name(self, _region):
+        __client = self.get_client('eu-north-1', 'ssm')
+        __name = f'/aws/service/global-infrastructure/regions/{_region}/longName'
+        __response = self.__client.get_parameter(Name=__name)
+        return __response.get('Parameter').get('Value')
+
+    def get_regions(self) -> dict:
+        __client = self.get_client()
+        __response = __client.describe_regions()
+        __regions = {}
+        [__regions.update({x.get('RegionName'): self.get_region_name(x.get('RegionName'))}) for x in __response.get('Regions')]
+        return __regions
 
 
 if __name__ == '__main__':
 
-    _regions_map = get_regions()
+    aws_ec2 = AwsEc2()
+
+    _regions_map = aws_ec2.get_regions()
 
     for region in _regions_map.keys():
 
-        client = get_client(region)
+        client = aws_ec2.get_client(region)
 
         _response = client.describe_addresses()
         _addresses = _response.get('Addresses', [])
+
         if not _addresses:
             continue
         for _address in _addresses:
